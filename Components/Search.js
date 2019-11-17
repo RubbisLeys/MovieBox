47c49b198c220598f704583d1a8ab8dfb7f4cb52
KIB <kibwilap@gmail.com>
2019-11-17 21:04:39 +0800

    the components folder contains the FilmItem.js and Search.js code files In our custom FilmItem component, we will recover the film and replace our default texts with the information of the film In our component custom Search, we will display a button to search for movies by entering the title of the film.

---
+++ Components/Search.js
@@ -0,0 +1,39 @@
+// Components/Search.js
+
+import React from 'react'
+import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+import films from '../Helpers/filmsData.js'
+import FilmItem from './FilmItem'
+
+class Search extends React.Component {
+  render() {
+    return (
+      <View style={{ styles.main_container }}>
+        <TextInput style={{ styles.textinput }} placeholder='movie title'/>
+        <Button style={{ height:50 }} title='Search' onPress={() => {}}/>
+        <FlatList
+          data={films}  //data={[{key: 'a'}, {key: 'b'}]}
+          KeyExtractor={(item) => item.id.toString()}  // KeyExtractor is a  Props
+          renderItem={({item}) => <FilmItem film={item}/>} // change {item.key} for {item.title}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  main_container: {
+    marginTop: 20,
+    flex: 1,
+  },
+  textinput:{
+    marginLeft: 5,
+    marginRigt: 5,
+    height: 50,
+    borderColor:'#000000',
+    borderWidth:1,
+    paddingLeft: 5
+  }
+})
+
+export default Search
