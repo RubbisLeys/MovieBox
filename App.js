d6ba08cb47a61ab6bd4d72d5bb6ce3a17d7fc5a1
KIB <kibwilap@gmail.com>
2019-11-17 21:21:14 +0800

    <App.js> has been modified and the <Search.js> component has been added to <App.js> now.

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
-//App.js
+// App.js
 
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Search from './Components/Search'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text> Hello Movies_&_Me ! </Text>
-      </View>
-    );
+      <Search/>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
