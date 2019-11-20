0a9e029f914d08e01b2f7ebb4d0b4884951fd692
KIB <kibwilap@gmail.com>
2019-11-20 20:34:01 +0800

    adding the file TMBDApi.js which contains the token which will allow us to recover movies from 'The Movie DataBase' API.

---
+++ API/TMDBApi.js
@@ -0,0 +1,11 @@
+// API/TMDBApi.js
+
+const API_TOKEN = '2f00d8e62562562c0883370e57011289';
+
+export function getFilmsFromApiWithSearchedText (text) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text  // This is an example of TMDB url
+  //const url = 'https://api.themoviedb.org/3/discover/movie?api_key=' + 2f00d8e62562562c0883370e57011289 +'&language=fr&query=' + text
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
